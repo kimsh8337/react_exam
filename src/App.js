24938e8dd2a2fef0bb4e877d09e30285d434fb7b
김석환 <kimsh8337@gmail.com>
2021-04-25 22:46:48 +0900

    patient_list

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Patientlist from './components/Patient_list';
+
+const App = () =>{
+  return (
+    <div>
+      <Patientlist />
+    </div>
+  )
+}
+
+export default App;
